869b9dc5767b3c5a380448e573113bf1b670b1dc
Sawady <sawady.faso@gmail.com>
2024-12-21 20:14:25 -0300

    testing on images

--- src/Game.hs
+++ src/Game.hs
@@ -1,55 +1,115 @@
-module Game (Game (..), newGame, loop) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
-import Control.Exception (SomeException, catch)
-import Control.Monad.State (MonadIO (liftIO), MonadState (put), StateT (runStateT), gets, modify, execStateT)
+module Game (game) where
+
+import Control.Lens
+import Control.Monad.State
 import Data.Text (Text)
 import Renderer (Renderer)
 import qualified SDL
+import qualified SDL.Image
 import System.Exit (exitFailure, exitSuccess)
 import Window (Window)
-import Prelude hiding (init)
+import Control.Monad.Except ( MonadError(catchError) )
 
-data Game = Game {_window :: Window, _renderer :: Renderer, _clean :: IO ()}
+-- Unified Game data type
+data Game = Game
+  { _window :: Window,
+    _renderer :: Renderer,
+    _background :: SDL.Texture,
+    _clean :: [IO ()]
+  }
 
-newGame :: Text -> SDL.WindowConfig -> IO Game
-newGame title config = do
-  ((window, renderer), clean) <- liftIO $ runStateT (initSDL title config) (return ())
-  execStateT loop $ Game {_window = window, _renderer = renderer, _clean = clean}
+makeLenses ''Game
 
-initSDL :: Text -> SDL.WindowConfig -> StateT (IO ()) IO (Window, Renderer)
+type Action a = StateT Game IO a
+
+-- Main game entry point
+game :: Text -> SDL.WindowConfig -> IO ()
+game title config = do
+  gameState <- initSDL title config
+  void $ execStateT gameloop gameState
+
+-- Initialize SDL and the game resources
+initSDL :: Text -> SDL.WindowConfig -> IO Game
 initSDL title config = do
-  put $ putStrLn "All Cleaned!"
+  -- Initialize SDL
+  SDL.initializeAll `andCatch` "Error initializing SDL2"
+  let cleanSDL = SDL.quit
 
-  try SDL.initializeAll "Error initializing SDL2"
-  modify (SDL.quit >>)
+  -- Initialize SDL Image
+  SDL.Image.initialize [SDL.Image.InitPNG] `andCatch` "Error initializing SDL2 Image"
+  let cleanImage = SDL.Image.quit
 
-  window <- try (SDL.createWindow title config) "Error creating Window"
-  modify (SDL.destroyWindow window >>)
+  -- Create the window
+  w <- SDL.createWindow title config `andCatch` "Error creating Window"
+  let cleanWindow = SDL.destroyWindow w
 
-  renderer <- try (SDL.createRenderer window (-1) SDL.defaultRenderer) "Error creating Renderer"
-  modify (SDL.destroyRenderer renderer >>)
+  -- Create the renderer
+  r <- SDL.createRenderer w (-1) SDL.defaultRenderer `andCatch` "Error creating Renderer"
+  let cleanRenderer = SDL.destroyRenderer r
 
-  return (window, renderer)
+  -- Load the background texture
+  b <- SDL.Image.loadTexture r "./resources/bg.png" `andCatch` "Error loading Texture"
+  let cleanTexture = SDL.destroyTexture b
 
-loop :: StateT Game IO ()
-loop = do
-  renderer <- gets _renderer
-  SDL.clear renderer
-  SDL.present renderer
-  SDL.delay 5000
-  exitClean
+  -- Return the fully constructed `Game` object
+  return $
+    Game
+      { _window = w,
+        _renderer = r,
+        _background = b,
+        _clean = [cleanTexture, cleanRenderer, cleanWindow, cleanImage, cleanSDL]
+      }
 
-try :: (MonadIO m) => IO a -> String -> m a
-try action errorMsg = liftIO $ catch action $ errorClean errorMsg
+-- Generalized error handling and cleanup
+andCatch :: IO a -> String -> IO a
+andCatch action errorMsg = catchError action (exitWith errorMsg)
 
-errorClean :: String -> SomeException -> IO a
-errorClean msg exception = do
-  putStrLn $ msg ++ ":"
-  print exception
+exitWith :: Show a => String -> a -> IO b
+exitWith errorMsg e = do
+  putStrLn $ errorMsg ++ ":"
+  print e
   exitFailure
 
-exitClean :: StateT Game IO ()
+-- Clean up resources and exit
+exitClean :: Action a
 exitClean = do
-  clean <- gets _clean
-  liftIO clean
+  liftIO $ putStrLn "Exit clean!"
+  actions <- use clean
+  liftIO $ sequence_ actions
   liftIO exitSuccess
+
+-- Handle SDL events
+handleEvent :: SDL.Event -> Action ()
+handleEvent event = case SDL.eventPayload event of
+  SDL.KeyboardEvent keyboardEvent
+    | SDL.keyboardEventKeyMotion keyboardEvent == SDL.Pressed ->
+        case SDL.keysymKeycode (SDL.keyboardEventKeysym keyboardEvent) of
+          SDL.KeycodeEscape -> exitClean
+          SDL.KeycodeQ -> exitClean
+          _ -> return ()
+  SDL.QuitEvent -> exitClean
+  _ -> return ()
+
+-- Game loop
+gameloop :: Action ()
+gameloop = do
+  -- Poll and handle events
+  SDL.pollEvents >>= mapM_ handleEvent
+
+  -- Render the frame
+  r <- use renderer
+  b <- use background
+  liftIO $ SDL.clear r
+  liftIO $ SDL.copy r b Nothing Nothing
+  liftIO $ SDL.present r
+
+  -- Delay for frame timing (16ms for ~60fps)
+  liftIO $ SDL.delay 16
+
+  -- Recursive call
+  gameloop
